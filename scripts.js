e47bf6fe82ae3ef1aa194bd7c07f7ad7adae93a1
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-06-15 16:48:12 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Executes when the DOM is fully loaded and parsed
 
-    // Add click event listeners to all links inside .ctext ul li a
     document.querySelectorAll('.ctext ul li a').forEach(function (link) {
         link.addEventListener('click', function (event) {
-            event.preventDefault(); // Prevent default link behavior (e.g., navigating to another page)
-            
-            // Remove 'active' class from all links
+            event.preventDefault(); 
             document.querySelectorAll('.ctext ul li a').forEach(function (link) {
                 link.classList.remove('active');
             });
             
-            // Add 'active' class to the clicked link
             link.classList.add('active');
 
-            showContent(event.target.textContent.toLowerCase()); // Call showContent with lowercase text content of clicked link
+            showContent(event.target.textContent.toLowerCase()); 
         });
     });
 
-    // Function to show the selected content section
     function showContent(section) {
-        // Hide all content sections (.skills, .experience, .education)
+      
         document.querySelectorAll('.ctext .skills, .ctext .experience, .ctext .education').forEach(function (content) {
-            content.classList.remove('content-visible'); // Remove 'content-visible' class from all sections
+            content.classList.remove('content-visible'); 
         });
 
-        // Show the selected content section
-        document.querySelector('.ctext .' + section).classList.add('content-visible'); // Add 'content-visible' class to selected section
+        document.querySelector('.ctext .' + section).classList.add('content-visible'); 
     }
 
-    // Optionally, show one section by default (e.g., 'skills')
     showContent('skills');
 });
 
